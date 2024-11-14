f3c155d55fedd8290bf72508c9575486c608d9aa
preethifredy <preethifrancise@gmail.com>
2024-11-14 10:42:59 +0530

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
